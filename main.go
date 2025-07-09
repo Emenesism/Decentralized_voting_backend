ad6ff42d9232079a882fdad9ad993ad6956288bd
em <mhdnris@gmail.com>
2025-07-09 23:13:21 +0330

    fix: correct formatting and improve readability in main function

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"github.com/emenesism/Decentralized-voting-backend/config"
-	"github.com/emenesism/Decentralized-voting-backend/router/http"
+	"fmt"
 
 	"github.com/charmbracelet/log"
+	"github.com/emenesism/Decentralized-voting-backend/config"
+	"github.com/emenesism/Decentralized-voting-backend/router/http"
+	"github.com/emenesism/Decentralized-voting-backend/service"
 )
 
-
-func main () {
+func main() {
 	config.Init()
 	log.Info("Config loaded")
+
+	service.InitContractService()
+
+	count, err := service.GetVotes("Alice")
+	fmt.Println(count, err)
+	if err != nil {
+		log.Fatal("Failed to read votes:", err)
+	}
+	log.Info("Votes for Alice:", count)
+
 	http.Init()
 	log.Info("Server has been running")
 }
