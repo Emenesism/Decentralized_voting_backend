2d08dbc1e230786aba37986b7f35deaed06940e0
em <mhdnris@gmail.com>
2025-07-10 16:38:47 +0330

    fix: initialize models before starting the contract service

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"github.com/emenesism/Decentralized-voting-backend/config"
 	"github.com/emenesism/Decentralized-voting-backend/router/http"
 	"github.com/emenesism/Decentralized-voting-backend/service"
+	"github.com/emenesism/Decentralized-voting-backend/models"
 )
 
 func main() {
 	config.Init()
 	log.Info("Config loaded")
-
+	models.Init()
+	log.Info("Model init successfuly")
 	service.InitContractService()
-
 	http.Init()
 }
+
